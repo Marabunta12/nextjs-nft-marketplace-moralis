ae795f63ca40a9d5edad4f6283f3dc1079b44594
Bartek <s210869@sggw.edu.pl>
2022-09-09 18:35:55 +0200

    added Header component

--- pages/sell-nft.js
+++ pages/sell-nft.js
@@ -10,7 +10,7 @@ export default function Home() {
                 <meta name="description" content="NFT Marketplace" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            Hi
+            Sell Page
         </div>
     );
 }
